7ffd402d4c01e559552602f102a2ba7befa0adb8
derekpierre <derek.pierre@gmail.com>
2022-10-27 16:06:13 -0400

    Some installation adjustments/cleanup for porter's new home.

---
+++ porter/__about__.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import, division, print_function
+
+"""
+WARNING: Do not modify this file.
+"""
+
+__all__ = [
+    "__title__", "__summary__", "__version__", "__author__", "__email__", "__license__", "__copyright__", "__url__"
+]
+
+__title__ = "porter"
+
+__url__ = "https://github.com/nucypher/nucypher-porter"
+
+__summary__ = 'Pipe for PRE on Threshold Network'
+
+__version__ = "0.0.1"
+
+__author__ = "NuCypher"
+
+__email__ = "dev@nucypher.com"
+
+__license__ = "GNU Affero General Public License, Version 3"
+
+__copyright__ = 'Copyright (C) 2022 NuCypher'
