d6c0610d734e144619bb848a848d3ec06f420c91
derekpierre <derek.pierre@gmail.com>
2022-11-08 12:02:56 -0500

    Remove native Porter TLS support and Basic authentication. Entites who run Porter should use nginx or some other equivalent for tls/authentication functionality.

--- porter/cli/main.py
+++ porter/cli/main.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 import click
 from nucypher.blockchain.eth.networks import NetworksInventory
 from nucypher.characters.lawful import Ursula
@@ -18,9 +16,6 @@ from nucypher.config.constants import TEMPORARY_DOMAIN
 
 from porter.cli.help import echo_version, echo_config_root_path, echo_logging_root_path
 from porter.cli.literature import (
-    PORTER_BASIC_AUTH_ENABLED,
-    PORTER_BASIC_AUTH_REQUIRES_HTTPS,
-    PORTER_BOTH_TLS_KEY_AND_CERTIFICATION_MUST_BE_PROVIDED,
     PORTER_CORS_ALLOWED_ORIGINS,
     PORTER_RUN_MESSAGE
 )
@@ -47,9 +42,6 @@ def porter_cli():
 @option_registry_filepath
 @option_min_stake
 @click.option('--http-port', help="Porter HTTP/HTTPS port for JSON endpoint", type=NETWORK_PORT, default=Porter.DEFAULT_PORT)
-@click.option('--tls-certificate-filepath', help="Pre-signed TLS certificate filepath", type=click.Path(dir_okay=False, exists=True, path_type=Path))
-@click.option('--tls-key-filepath', help="TLS private key filepath", type=click.Path(dir_okay=False, exists=True, path_type=Path))
-@click.option('--basic-auth-filepath', help="htpasswd filepath for basic authentication", type=click.Path(dir_okay=False, exists=True, resolve_path=True, path_type=Path))
 @click.option('--allow-origins', help="The CORS origin(s) comma-delimited list of strings/regexes for origins to allow - no origins allowed by default", type=click.STRING)
 @click.option('--dry-run', '-x', help="Execute normally without actually starting Porter", is_flag=True)
 @click.option('--eager', help="Start learning and scraping the network before starting up other services", is_flag=True, default=True)
@@ -61,9 +53,6 @@ def run(general_config,
         registry_filepath,
         min_stake,
         http_port,
-        tls_certificate_filepath,
-        tls_key_filepath,
-        basic_auth_filepath,
         allow_origins,
         dry_run,
         eager):
@@ -71,17 +60,6 @@ def run(general_config,
     emitter = setup_emitter(general_config, banner=BANNER)
 
     # HTTP/HTTPS
-    if bool(tls_key_filepath) ^ bool(tls_certificate_filepath):
-        raise click.BadOptionUsage(option_name='--tls-key-filepath, --tls-certificate-filepath',
-                                   message=click.style(PORTER_BOTH_TLS_KEY_AND_CERTIFICATION_MUST_BE_PROVIDED, fg="red"))
-
-    is_https = (tls_key_filepath and tls_certificate_filepath)
-
-    # check authentication
-    if basic_auth_filepath and not is_https:
-        raise click.BadOptionUsage(option_name='--basic-auth-filepath',
-                                   message=click.style(PORTER_BASIC_AUTH_REQUIRES_HTTPS, fg="red"))
-
     if federated_only:
         if not teacher_uri:
             raise click.BadOptionUsage(option_name='--teacher',
@@ -131,16 +109,9 @@ def run(general_config,
         allow_origins_list = allow_origins.split(",")  # split into list of origins to allow
         emitter.message(PORTER_CORS_ALLOWED_ORIGINS.format(allow_origins=allow_origins_list), color='green')
 
-    if basic_auth_filepath:
-        emitter.message(PORTER_BASIC_AUTH_ENABLED, color='green')
-
     controller = PORTER.make_web_controller(crash_on_error=False,
-                                            htpasswd_filepath=basic_auth_filepath,
                                             cors_allow_origins_list=allow_origins_list)
-    http_scheme = "https" if is_https else "http"
-    message = PORTER_RUN_MESSAGE.format(http_scheme=http_scheme, http_port=http_port)
+    message = PORTER_RUN_MESSAGE.format(http_port=http_port)
     emitter.message(message, color='green', bold=True)
     return controller.start(port=http_port,
-                            tls_key_filepath=tls_key_filepath,
-                            tls_certificate_filepath=tls_certificate_filepath,
                             dry_run=dry_run)
