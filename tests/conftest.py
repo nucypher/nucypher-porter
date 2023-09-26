1a5741c3f9bf73f871ddd02e821504a1752745c4
derekpierre <derek.pierre@gmail.com>
2023-09-26 16:10:30 -0400

    Remove use of Economics/EconomicsFactory.

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,6 @@ import pytest
 from click.testing import CliRunner
 from eth_typing import ChecksumAddress
 from eth_utils import to_checksum_address
-from nucypher.blockchain.economics import Economics, EconomicsFactory
 from nucypher.blockchain.eth.actors import Operator
 from nucypher.blockchain.eth.agents import (
     ContractAgency,
@@ -138,18 +137,8 @@ def staking_providers(testerchain, test_registry, monkeymodule):
     return testerchain.stake_providers_accounts
 
 
-@pytest.fixture(scope='module')
-def application_economics():
-    economics = Economics()
-    return economics
-
-
 @pytest.fixture(scope='module', autouse=True)
-def mock_contract_agency(module_mocker, application_economics):
-
-    # Patch
-    module_mocker.patch.object(EconomicsFactory, 'get_economics', return_value=application_economics)
-
+def mock_contract_agency():
     from tests.mock.agents import MockContractAgency
 
     # Monkeypatch # TODO: Use better tooling for this monkeypatch?
