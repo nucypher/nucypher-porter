b06a2f8c1c939bddab887362eefca9a06726b1b2
derekpierre <derek.pierre@gmail.com>
2026-01-20 11:46:52 -0500

    Update copyright year.

--- porter/__about__.py
+++ porter/__about__.py
@@ -22,4 +22,4 @@ __email__ = "dev@nucypher.com"
 
 __license__ = "GNU Affero General Public License, Version 3"
 
-__copyright__ = 'Copyright (C) 2022 NuCypher'
+__copyright__ = "Copyright (C) 2021-2026 NuCypher"
